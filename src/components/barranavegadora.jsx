b3a03dcf1e6b0bbf8e7b706f48af6cd0d4f9a9c5
Mathi4s <mathiastrasmonte2022@gmail.com>
2023-01-21 23:16:15 -0500

    a

--- src/components/barranavegadora.jsx
+++ src/components/barranavegadora.jsx
@@ -3,12 +3,12 @@ import './Navstyle.css'
 
 export const Navbar = () =>{
     return(
-        <nav className='flex justify-between content-center items-center p-4 bg-black'>
-            <h1 className='p-3 text-3xl text-black font-semibold'>MATHIAS</h1>
-            <ul className='flex gap-10 pr-8 list-none'>
-                <li><Link to="" className='hover:text-stone-500 '>Principal</Link></li>
-                <li><Link to="/home" className='hover:text-stone-500'>SADAS</Link></li>
-                <li><Link to="/about" className='hover:text-stone-500'>About</Link></li>
+        <nav className='flex justify-between content-center items-center p-4 bg-[#567189] pr-40 pl-40'>
+            <h1 className='p-3 text-3xl text-[#FAD6A5] font-semibold hover:cursor-default'>MATHIAS</h1>
+            <ul className='flex gap-20 list-none'>
+                <li><Link to="" className='hover:text-[#FAD6A5] '>Principal</Link></li>
+                <li><Link to="/home" className='hover:text-[#FAD6A5]'>SADAS</Link></li>
+                <li><Link to="/about" className='hover:text-[#FAD6A5]'>Sobre Nosotros</Link></li>
             </ul>
         </nav>
         )
